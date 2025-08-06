7c8ccd186081c83713485ca8c8d0c5b314f243f6
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-05 21:43:37 -0400

    invoke showSurprise after randomTime

--- surprise.js
+++ surprise.js
@@ -3,3 +3,10 @@ const surpriseSection = document.getElementById('surprise')
 function showSurprise() {
   surpriseSection.textContent = '🎉 Surprise! 🎉'
 }
+
+//generate a random time
+const randomTime = Math.random() * 4000
+
+//invoke showSurprise() after a random period of time
+//setTimeOut(callback, time in milliseconds)
+setTimeout(showSurprise, randomTime)
