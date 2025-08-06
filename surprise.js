0b0b0b691e9d9ff31d37d2f939110ce69802f6ad
Brian Crowe <brian.j.crowe@protonmail.com>
2025-08-05 21:44:17 -0400

    alter setTimeout call to work with anonymous arrow function

--- surprise.js
+++ surprise.js
@@ -9,4 +9,7 @@ const randomTime = Math.random() * 4000
 
 //invoke showSurprise() after a random period of time
 //setTimeOut(callback, time in milliseconds)
-setTimeout(showSurprise, randomTime)
+//setTimeout(showSurprise, randomTime)
+
+//modify to use anonymous arrow function instead of showSurprise
+setTimeout(() => (surpriseSection.textContent = '🎉 Surprise! 🎉'), randomTime)
